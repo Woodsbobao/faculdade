3f0b87db3d299e8e80f49a254c746ba513f08fbb
rafael <141762633+Woodsbobao@users.noreply.github.com>
2026-03-03 10:38:48 -0300

    exercicio de variaveis

---
+++ python/aula03/variaveis.py
@@ -0,0 +1,14 @@
+#VARIAVEIS E TIPOS DE DADOS: 
+#em pyhon, vc não precisa declarar o tipo da variavel antes de usála. o tipo é inferido automaticamente
+#pelo interpretador. a variavel é declarada no padrão snake_case.
+#string: texto, ex: nome = "João"
+#int: números, inteiros ex: idade = 30
+#float: números decimais, ex: altura = 1.75
+#boolean: verdadeiro ou falso, ex: is_student = True
+
+nome = "rafael" #string
+email = "rafaelsigma@gmail.com" #string
+cidade = "rio de janeiro" #string
+cpf = 12312312312 #number-int, sem a aspas e os pontos do cpf, é int, com é string.
+salario = 2500.50 #number-float
+casado = False #boolean
